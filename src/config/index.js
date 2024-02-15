17fef8588a242e8b06974c693f00f6c79bf03ca7
top329 <tanakayuki329@outlook.com>
2024-02-16 06:13:11 +1000

    base url updated

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-// export const BASE_URL =
-//   'https://copy-trading-platform-frontend-git-main-jordon-chens-projects.vercel.app/';
-export const BASE_URL = 'http://localhost:5173/';
+export const BASE_URL =
+  'https://copy-trading-platform-frontend-git-main-jordon-chens-projects.vercel.app/';
+// export const BASE_URL = 'http://localhost:5173/';
--- src/utils/api.js
+++ src/utils/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  // baseURL: 'https://tradecopiersignalsbackend.onrender.com/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://tradecopiersignalsbackend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
